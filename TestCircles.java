c0418ca918a2fe2f5230753cbc3c789572fa03a8
emmachen12 <emc1207pi@gmail.com>
2024-05-15 14:30:47 -0400

    y

--- TestCircles.java
+++ TestCircles.java
@@ -2,5 +2,15 @@ public class TestCircles {
     public static void main (String [] args){
 
     Circle izzy = new Circle (3.0);
-    double cicum = izzy getC ();
+
+    double circum = izzy.getC();
+    System.out.println("Circumference: " + circum);
+
+    double area = izzy.getArea();
+
+    izzy.changeR(1.0);
+
+    double area2 = izzy.getArea();
+    System.out.println("New area: " + area2);
+}
 }
